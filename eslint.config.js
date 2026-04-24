81df29eebf1d724d2939cbfd8123b86e9848a940
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2026-04-25 04:22:35 +0530

    chore: link to Foundry fleet + apply standards

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,2 @@
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-
-export default [
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2022,
-      sourceType: "module",
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "@typescript-eslint/no-explicit-any": "warn",
-      "no-use-before-define": "off",
-      "@typescript-eslint/no-use-before-define": ["warn", { "functions": false, "classes": false, "variables": true }],
-      "no-console": "off",
-    },
-  },
-  {
-    ignores: ["node_modules/", "dist/", "out/", ".next/", "target/", "**/*.js", "**/*.mjs"],
-  },
-];
+import config from "@saas-maker/eslint-config/";
+export default config;
